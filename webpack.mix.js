c9f7288a0eeeeb2f36dabbc054163c7b43b52fd7
Mario Patronelli <mario@raffles.com.ar>
2018-12-06 16:09:57 +0000

    Add sass 7-1 pattern and update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.copy('resources/img/', 'public/img/')
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/main.scss', 'public/css/app.css')
+
+if (mix.inProduction()) {
+    mix.version()
+}
