480d44882232b3f525c189dd25150ddfa5c36f08
Mario Patronelli <mario@raffles.com.ar>
2018-12-06 22:57:42 +0000

    Fix eslint shown errors and delete ExampleComponent.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap")
 
-window.Vue = require('vue');
+import Vue from "vue"
 
 /**
  * The following block of code may be used to automatically register your
@@ -17,10 +17,8 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const files = require.context("./", true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split("/").pop().split(".")[0], files(key)))
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +27,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
  */
 
 const app = new Vue({
-    el: '#app'
-});
+    el: "#app"
+})
+
+export default app
--- resources/js/components/passport/AuthorizedClients.vue
+++ resources/js/components/passport/AuthorizedClients.vue
@@ -5,103 +5,115 @@
 </style>
 
 <template>
-    <div>
-        <div v-if="tokens.length > 0">
-            <div class="card card-default">
-                <div class="card-header">Authorized Applications</div>
-
-                <div class="card-body">
-                    <!-- Authorized Tokens -->
-                    <table class="table table-borderless mb-0">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Scopes</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr v-for="token in tokens">
-                                <!-- Client Name -->
-                                <td style="vertical-align: middle;">
-                                    {{ token.client.name }}
-                                </td>
-
-                                <!-- Scopes -->
-                                <td style="vertical-align: middle;">
-                                    <span v-if="token.scopes.length > 0">
-                                        {{ token.scopes.join(', ') }}
-                                    </span>
-                                </td>
-
-                                <!-- Revoke Button -->
-                                <td style="vertical-align: middle;">
-                                    <a class="action-link text-danger" @click="revoke(token)">
-                                        Revoke
-                                    </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  <div>
+    <div v-if="tokens.length > 0">
+      <div class="card card-default">
+        <div class="card-header">
+          Authorized Applications
         </div>
+
+        <div class="card-body">
+          <!-- Authorized Tokens -->
+          <table class="table table-borderless mb-0">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Scopes</th>
+                <th />
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr
+                v-for="token in tokens"
+                :key="token.id"
+              >
+                <!-- Client Name -->
+                <td style="vertical-align: middle;">
+                  {{ token.client.name }}
+                </td>
+
+                <!-- Scopes -->
+                <td style="vertical-align: middle;">
+                  <span v-if="token.scopes.length > 0">
+                    {{ token.scopes.join(', ') }}
+                  </span>
+                </td>
+
+                <!-- Revoke Button -->
+                <td style="vertical-align: middle;">
+                  <a
+                    class="action-link text-danger"
+                    @click="revoke(token)"
+                  >
+                    Revoke
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        /*
+import axios from "axios"
+
+export default {
+    /*
          * The component's data.
          */
-        data() {
-            return {
-                tokens: []
-            };
-        },
+    data() {
+        return {
+            tokens: []
+        }
+    },
 
-        /**
+    /**
          * Prepare the component (Vue 1.x).
          */
-        ready() {
-            this.prepareComponent();
+    ready() {
+        this.prepareComponent()
+    },
+
+    /**
+         * Prepare the component (Vue 2.x).
+         */
+    mounted() {
+        this.prepareComponent()
+    },
+
+    methods: {
+        /**
+             * Prepare the component (Vue 2.x).
+             */
+        prepareComponent() {
+            this.getTokens()
         },
 
         /**
-         * Prepare the component (Vue 2.x).
-         */
-        mounted() {
-            this.prepareComponent();
-        },
-
-        methods: {
-            /**
-             * Prepare the component (Vue 2.x).
-             */
-            prepareComponent() {
-                this.getTokens();
-            },
-
-            /**
              * Get all of the authorized tokens for the user.
              */
-            getTokens() {
-                axios.get('/oauth/tokens')
-                        .then(response => {
-                            this.tokens = response.data;
-                        });
-            },
+        getTokens() {
+            axios.get("/oauth/tokens")
+                .then(response => {
+                    return this.tokens = response.data
+                })
+                .catch(error => console.error(error))
+        },
 
-            /**
+        /**
              * Revoke the given token.
              */
-            revoke(token) {
-                axios.delete('/oauth/tokens/' + token.id)
-                        .then(response => {
-                            this.getTokens();
-                        });
-            }
+        revoke(token) {
+            axios.delete("/oauth/tokens/" + token.id)
+                .then(()=> {
+                    return this.getTokens()
+                })
+                .catch(error => console.error(error))
         }
     }
+}
 </script>
