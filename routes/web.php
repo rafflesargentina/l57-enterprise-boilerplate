e30bf2f5b3eb8c245adc602f8d3626ae27eb37a3
Mario Patronelli <mario@raffles.com.ar>
2018-12-07 01:37:12 +0000

    Add logout GET route

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/logout', 'Auth\LoginController@logout');
 
 Route::post('/contact', 'ContactController');
