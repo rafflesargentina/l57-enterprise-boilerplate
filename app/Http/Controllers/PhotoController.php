9833c2f03a1f7cec225c35ba21f627a32beccdbb
Mario Patronelli <mario@raffles.com.ar>
2019-01-23 21:39:01 +0000

    Modify destroy method logic to delete stored files

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,11 +4,58 @@ namespace Raffles\Http\Controllers;
 
 use Raffles\Repositories\PhotoRepository;
 
+use DB;
+use Illuminate\Http\Request;
 use RafflesArgentina\ResourceController\ApiResourceController;
+use Storage;
 
 class PhotoController extends ApiResourceController
 {
     protected $repository = PhotoRepository::class;
 
     protected $resourceName = 'photos';
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Request $request The request object.
+     * @param string  $key     The model key.
+     *
+     * @throws ResourceControllerException
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Request $request, $key)
+    {
+        $this->getFormRequestInstance();
+
+        $model = $this->findFirstByKey($key);
+
+        if (!$model) {
+            return $this->validNotFoundJsonResponse();
+        }
+
+        $location = $model->{$this->getLocationColumn()};
+        if (Storage::exists($location)) {
+            Storage::delete($location);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $this->repository->delete($model);
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            $message = $this->destroyFailedMessage($key, $e->getMessage());
+            throw new ResourceControllerException($message);
+        }
+
+        DB::commit();
+
+        $message = $this->destroySuccessfulMessage($key);
+        $data = [$model];
+
+        return $this->validSuccessJsonResponse($message, $data);
+    }
 }
