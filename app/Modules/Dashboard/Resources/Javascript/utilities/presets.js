5c3e0f45b83161750d8dc99d2572fab43ab9ba58
Mario Patronelli <mario@raffles.com.ar>
2019-02-10 20:38:08 +0000

    Add Dashboard js utilities

---
+++ app/Modules/Dashboard/Resources/Javascript/utilities/presets.js
@@ -0,0 +1,10 @@
+export const DATE_RANGES = {
+    "30days": "30 días",
+    "60days": "60 días",
+    "365days": "365 días",
+    "today": "Hoy",
+    "thisWeek": "Esta semana",
+    "thisMonth": "Este mes",
+    "thisQuarter": "Este trimestre",
+    "thisYear": "Este año",
+}
