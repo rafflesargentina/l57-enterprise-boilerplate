285b7f898c34bb739f86287a6d632e6cb56b7f8f
Mario Patronelli <mario@raffles.com.ar>
2019-02-10 21:15:33 +0000

    Refactor Dashboard middleware

--- app/Modules/Dashboard/Resources/Javascript/router/middleware.js
+++ app/Modules/Dashboard/Resources/Javascript/router/middleware.js
@@ -2,7 +2,7 @@ import store from "@/store"
 
 export const canAccessDashboard = (to, from, next) => {
     var user = store.state.auth.user
-    if (store.getters["auth/isAuthenticated"] && user.roles.map(item => item["slug"]).includes("admin")) {
+    if (store.getters["auth/isAuthenticated"] && store.getters["auth/isAdmin"]) {
         return next()
     }
 
