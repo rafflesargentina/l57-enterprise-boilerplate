521b2a6cbdf9e8e06188d575dcb99ced524744ee
Mario Patronelli <mario@raffles.com.ar>
2019-02-15 20:28:12 +0000

    Remove unused var

--- app/Modules/Dashboard/Resources/Javascript/router/middleware.js
+++ app/Modules/Dashboard/Resources/Javascript/router/middleware.js
@@ -1,7 +1,6 @@
 import store from "@/store"
 
 export const canAccessDashboard = (to, from, next) => {
-    var user = store.state.auth.user
     if (store.getters["auth/isAuthenticated"] && store.getters["auth/isAdmin"]) {
         return next()
     }
