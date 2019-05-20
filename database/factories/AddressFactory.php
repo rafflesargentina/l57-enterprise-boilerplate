c7c392781aa23e1a97d271c1b857368b7d6fdcb9
Mario Patronelli <mario@raffles.com.ar>
2019-05-20 03:27:22 +0000

    Update AddressFactory.php

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -8,6 +8,7 @@ $factory->define(Raffles\Models\Address::class, function (Faker $faker) {
         'addressable_type' => 'Raffles\Models\User',
         'country' => $faker->country,
         'door_number' => rand(1,25000),
+        'featured' => rand(0,1),
         'floor_number' => rand(1,25),
         'locality' => $faker->city,
         'state' => $faker->state,
