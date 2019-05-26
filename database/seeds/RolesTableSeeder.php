6737892dc9d67b4182ff316475ccbefd0bcf8e8e
Mario Patronelli <mario@raffles.com.ar>
2019-05-26 23:16:01 +0000

    Add RolesTableSeeder.php

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Caffeinated\Shinobi\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            ['name' => 'Admin', 'slug' => 'admin']
+        ];
+
+        foreach ($roles as $role) Role::create($role);
+    }
+}
